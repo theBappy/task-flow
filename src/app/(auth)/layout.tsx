886d5abfa4800f68d5ada6f917948924ce5354be
theBappy <abappy575@gmail.com>
2025-09-09 11:46:10 +0600

    for sign in and sign up used zod and hook form resolver

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const AuthLayout = ({ children }: Props) => {
+  const pathname = usePathname();
+  const isSignIn = pathname === "/sign-in";
+
+  return (
+    <main className="bg-neutral-100 min-h-screen">
+      <div className="mx-auto max-w-screen-2xl p-4">
+        <nav className="flex justify-between items-center">
+          <Image src="/logo.svg" alt="log" width={152} height={56} />
+          <Button variant="secondary" asChild>
+            <Link href={isSignIn ? "/sign-up" : "/sign-in"}>
+              {isSignIn ? "Sign Up" : "Login"}
+            </Link>
+          </Button>
+        </nav>
+        <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+          {children}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default AuthLayout;
---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,112 @@
+import { DottedSeparator } from "@/components/custom/dotted-separator";
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(1, "Required"),
+});
+
+export const SignInCard = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <div>
+      <Card className="w-full h-full md:w-[486px] border-none shadow-none">
+        <CardHeader className="flex items-center justify-center text-center p-7">
+          <CardTitle className="text-2xl">Welcome back!</CardTitle>
+        </CardHeader>
+        <div className="px-7">
+          <DottedSeparator />
+        </div>
+        <CardContent className="p-7">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                name="email"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="email"
+                        placeholder="Enter email address"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                name="password"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="password"
+                        placeholder="Enter your password"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button disabled={false} size="lg" className="w-full">
+                Login
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+        <div className="px-7">
+          <DottedSeparator />
+        </div>
+        <CardContent className="p-7 flex flex-col gap-y-4">
+          <Button
+            variant="secondary"
+            size="lg"
+            className="w-full"
+            disabled={false}
+          >
+            <FcGoogle className="mr-2 size-5" />
+            Login with Google
+          </Button>
+          <Button
+            variant="secondary"
+            size="lg"
+            className="w-full"
+            disabled={false}
+          >
+            <FaGithub className="mr-2 size-5" />
+            Login with Github
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
