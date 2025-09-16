44ac9fe663e712f4b342edae1c138ae8a24522e3
theBappy <abappy575@gmail.com>
2025-09-16 13:06:03 +0600

    data filtering and also ui implemented

--- src/features/tasks/api/use-get-task.ts
+++ src/features/tasks/api/use-get-task.ts
@@ -1,16 +1,44 @@
 import { client } from "@/lib/rpc/rpc";
 import { useQuery } from "@tanstack/react-query";
+import { TaskStatus } from "../types";
 
 interface UseGetTaskProps {
   workspaceId: string;
+  projectId?: string | null;
+  status?: TaskStatus | null;
+  search?: string | null;
+  assigneeId?: string | null;
+  dueDate?: string | null;
 }
 
-export const useGetTask = ({ workspaceId }: UseGetTaskProps) => {
+export const useGetTask = ({
+  workspaceId,
+  projectId,
+  status,
+  assigneeId,
+  search,
+  dueDate,
+}: UseGetTaskProps) => {
   const query = useQuery({
-    queryKey: ["tasks", workspaceId],
+    queryKey: [
+      "tasks",
+      workspaceId,
+      projectId,
+      status,
+      search,
+      assigneeId,
+      dueDate,
+    ],
     queryFn: async () => {
       const response = await client.api.tasks.$get({
-        query: { workspaceId },
+        query: {
+          workspaceId,
+          projectId: projectId ?? undefined,
+          status: status ?? undefined,
+          search: search ?? undefined,
+          dueDate: dueDate ?? undefined,
+          assigneeId: assigneeId ?? undefined,
+        },
       });
 
       if (!response.ok) {
@@ -19,7 +47,6 @@ export const useGetTask = ({ workspaceId }: UseGetTaskProps) => {
 
       const { data } = await response.json();
       return data;
-
     },
   });
   return query;
